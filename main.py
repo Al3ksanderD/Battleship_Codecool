1a4b872042f437d3626e17d4b4b5557a186e8c07
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-06-26 16:53:31 +0200

    Initial state of the file structure of the project.

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from board import *
+from menu import *
+from tests import *
+from coordinates import *
+
 
 # Constant values
 HEIGHT = 10  # Póki co ustawiłem jak stałe wartości ale jak dorobię funkcję do zmiany rozmiaru
 WIDTH = 10   # planszy to to zmienię
 BLUE = (57,197,243)
 
+
 # Functions
+
+def game_flow(name):
+    names = get_user_names()
+
+
 def colored(rgb, text): # RGB Colors function
     r = rgb[0]
     g = rgb[1]
     b = rgb[2]
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
+
 
 def draw_grid(width, height):
     """
@@ -31,6 +43,6 @@ print(BLUE[0])
 draw_grid(WIDTH, HEIGHT)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('')
+    test()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ tests.py
@@ -0,0 +1,2 @@
+def test():
+    print("That's a test")
