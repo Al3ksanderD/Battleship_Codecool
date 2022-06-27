528b4af4a8db869383240344ac6e5b436a66286e
jokosiacka <jo.kosiacka@gmail.com>
2022-06-27 23:27:57 +0200

    Main menu

--- menu.py
+++ menu.py
@@ -1,3 +1,22 @@
+import time
+
+
+def menu():
+    print("****** Main Menu ******\n")
+    time.sleep(1)
+    print("1. HUMAN VS HUMAN")
+    time.sleep(1)
+    print("2. HUMAN VS RANDOM AI")
+    time.sleep(1)
+    print("3. RANDOM AI VS RANDOM AI")
+    time.sleep(1)
+    print("4. EXIT\n")
+    time.sleep(1)
+
+
+option = int(input("Please choose from presented options: "))
+
+
 def get_player_names():
     pass
 
