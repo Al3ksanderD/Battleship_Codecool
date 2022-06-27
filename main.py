4bc89fd5233fadbbe8f8adb61afeb497703c70e1
Al3ksanderD <olo1045@wp.pl>
2022-06-27 00:07:56 +0000

    Added functionality: place_ship Changed grid for two seperate grids: the friendly grid and the enemy grid.

--- main.py
+++ main.py
@@ -2,22 +2,13 @@ from board import *
 from menu import *
 from tests import *
 from coordinates import *
+
 # Constant values
 HEIGHT = 10  # Póki co ustawiłem jak stałe wartości ale jak dorobię funkcję do zmiany rozmiaru
 WIDTH = 10  # planszy to to zmienię
 BLUE = (57, 197, 243)
-RED = (255,37,88)
-
-
-# Functions
-
-
-
-
-# Constant values
-HEIGHT = 10  # Póki co ustawiłem jak stałe wartości ale jak dorobię funkcję do zmiany rozmiaru
-WIDTH = 10   # planszy to to zmienię
-BLUE = (57, 197, 243)
+RED = (255, 37, 88)
+GREEN = (71, 192, 70)
 
 
 # Functions
@@ -33,39 +24,101 @@ def colored(rgb, text):  # RGB Colors function
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
 
-def place_ships(size, startingXY, secondXY, position):
+def ai_ship_generator():
+    pass
+
+
+def place_ships(size, startingXY, lastXY, position, player):
     """
 
     Size - length of the ship
     StartingXY - the starting coordinate of the ship
     SecondXY - the coordinate of the adjacent ship tile
     Position - Horizontal or Vertical
+    Player - to what player does the ships belong to
     """
+    startX = startingXY[0]
+    startY = startingXY[1] - 1
+    lastX = lastXY[0]
+    lastY = lastXY[1] - 1
+    colored_value = colored(GREEN, "S")
+    if player == 1:
+        if position == "H":
+
+            check_direction = startX - lastX
+            if check_direction > 0:
+                for cord in range(size):
+                    grid_friendly[startY][startX] = colored_value
+                    startX -= 1
+
+            else:
+                for cord in range(size):
+                    grid_friendly[startY][startX] = colored_value
+                    startX += 1
+                pass
+        else:
+            check_direction = startY - lastY
+            if check_direction > 0:
+                for cord in range(size):
+                    grid_friendly[startY][startX] = colored_value
+                    startY -= 1
+
+            else:
+                for cord in range(size):
+                    grid_friendly[startY][startX] = colored_value
+                    startY += 1
+                pass
+    if player == 2:
+        if position == "H":
+
+            check_direction = startX - lastX
+            if check_direction > 0:
+                for cord in range(size):
+                    grid_enemy[startY][startX] = colored_value
+                    startX -= 1
+
+            else:
+                for cord in range(size):
+                    grid_enemy[startY][startX] = colored_value
+                    startX += 1
+                pass
+        else:
+            check_direction = startY - lastY
+            if check_direction > 0:
+                for cord in range(size):
+                    grid_enemy[startY][startX] = colored_value
+                    startY -= 1
+
+            else:
+                for cord in range(size):
+                    grid_enemy[startY][startX] = colored_value
+                    startY += 1
+                pass
 
 
+        """
+        Tą funkcję będzie trzeba trochę skrócić bo się mnóstwo
+        kodu powtarza ale zajmę się tym jak już cała gra będzie działać
+        """
 
-def draw_grid(width, height, turn):
+    else:
+        pass
+
+
+def draw_grid(width, grid, turn):
     """
     W - Water
     S - Ship
     M - Missed Shot
     T - Target Hit
     """
+    global water, grid_friendly, grid_enemy, alphabet
     alphabet = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'K',
                 13: 'M', 14: 'N',
                 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y',
                 26: 'Z'
                 }
-    global water, grid_friendly, grid_enemy
-    water = colored(BLUE, 'W')
-    grid_friendly = [[water] * width for i in range(height)]
-    grid_enemy = [[water] * width for i in range(height)]
 
-
-    if turn == 1:
-        grid = grid_friendly
-    else:
-        grid = grid_enemy
     counter = 1
     print("     ", end="")
     for x in range(width):
@@ -74,22 +127,27 @@ def draw_grid(width, height, turn):
     print("")
     for x in grid:
         colored_value = colored(RED, alphabet[counter])
-        print(colored_value, end =" |")
+        print(colored_value, end=" |")
         counter += 1
         for y in x:
-                print(f" {y}", end="|")
+            print(f" {y}", end="|")
         print("")
-        print("   ",end="")
+        print("   ", end="")
         for y in x:
             print("|---", end="")
 
         print("|")
 
 
+# Zmienne są tu tymczasowo jak wymyślę co z nimi zrobić przerzucę je gdzieś indziej
+water = colored(BLUE, 'W')
+grid_friendly = [[water] * WIDTH for i in range(HEIGHT)]
+grid_enemy = [[water] * WIDTH for i in range(HEIGHT)]
 
-
-print(BLUE[0])
-draw_grid(WIDTH, HEIGHT, 1)
+draw_grid(WIDTH, grid_friendly, 1)
+place_ships(3, (3, 8), (3, 5), "V", 2)
+draw_grid(WIDTH, grid_enemy, 1)
+# print(list(alphabet.keys())[list(alphabet.values()).index("A")])
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     test()
