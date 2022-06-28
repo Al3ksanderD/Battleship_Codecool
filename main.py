e620b471fab51bc72b48cc146eb31af4dc2d254c
ola-sk <50403262+ola-sk@users.noreply.github.com>
2022-06-28 22:27:27 +0200

    Fix typo.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ BLUE = (57, 197, 243)
 RED = (255, 37, 88)
 GREEN = (71, 192, 70)
 
-global water, grid_friendly, grid_enemy, alphabe
+global water, grid_friendly, grid_enemy, alphabet
 alphabet = {
         1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L', 13: 'M',
         14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y',
